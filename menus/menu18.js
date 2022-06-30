d1a188aa88e5098c95f899d211501f24cdfb8aca
seunome <you@example.com>
2022-06-30 16:07:49 -0300

    First commit

---
+++ menus/menu18.js
@@ -0,0 +1,18 @@
+const hospedar = (prefix) => {
+return `
+
+╭═• ೋ✮๑🥂๑✮ೋ •═╮              
+           😈𝐌𝐄𝐍𝐔 +18😈
+╰═• ೋ✮๑🥂๑✮ೋ •═╯
+╎✮ۣۜۜ͜͡ 💋 ${prefix}porno "NOME"
+╎✮ۣۜۜ͜͡ 💋 ${prefix}xvideos "LINK"
+╎✮ۣۜۜ͜͡ 💋 ${prefix}hentai
+╎✮ۣۜۜ͜͡ 💋 ${prefix}plaq "SEU NM"
+╎✮ۣۜۜ͜͡ 💋 ${prefix}plaq2 "SEU NM"
+╎✮ۣۜۜ͜͡ 💋 ${prefix}plaq4 "SEU NM"
+╎✮ۣۜۜ͜͡ 💋 ${prefix}plaq5 "SEU NM"
+╰╼━═━━≺🎠≻━━═━╾╯
+`
+}
+
+exports.hospedar = hospedar 
---
+++ qrcode.sh
@@ -0,0 +1,3 @@
+rm -rf auth_info_multi.json
+rm -rf baileys_store_multi.json
+sh start.sh
